db08235bb78b048d954f1d94b146117ca7b33684
Bolormaa1206 <159024060+Bolormaa1206@users.noreply.github.com>
2026-04-03 09:52:47 +0800

    UserProfile.java

--- user-json-service/src/main/java/mn/edu/num/userjsonservice/model/UserProfile.java
+++ user-json-service/src/main/java/mn/edu/num/userjsonservice/model/UserProfile.java
@@ -1,61 +1,43 @@
 package mn.edu.num.userjsonservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
 public class UserProfile {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String bio;
+    private String phone;
+    private String profileImageUrl;
 
-	private String id;
-	private String name;
-	private String email;
-	private String bio;
-	private String phone;
+    public UserProfile() {}
 
-	public UserProfile() {
-	}
+    public UserProfile(String id, String name, String email, String bio, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bio = bio;
+        this.phone = phone;
+    }
 
-	public UserProfile(String id, String name, String email, String bio, String phone) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.bio = bio;
-		this.phone = phone;
-	}
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-	public String getId() {
-		return id;
-	}
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-	public String getName() {
-		return name;
-	}
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getBio() {
-		return bio;
-	}
-
-	public void setBio(String bio) {
-		this.bio = bio;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public String getProfileImageUrl() { return profileImageUrl; }
+    public void setProfileImageUrl(String profileImageUrl) { this.profileImageUrl = profileImageUrl; }
 }
